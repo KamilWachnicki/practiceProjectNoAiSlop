1502f8cf6279714cbeac5f2a3b892ef4525a24fb
Kamil Wachnicki <kamilwachnicki321@gmail.com>
2025-11-07 16:09:33 +0100

    added subs for people

--- src/app/people/script.js
+++ src/app/people/script.js
@@ -1,19 +1,22 @@
 const querySelector = document.querySelector.bind(document)
 const createElement = document.createElement.bind(document)
 
-async function read() {
-    const container = document.getElementById('cardsContainer');
+async function read(acceptedName = ' ') {
+    const container = querySelector('#cardsContainer');
     try {
         const res = await fetch("../../data/people.json")
         const data = await res.json()
         data.forEach(person => {
-            const card = document.createElement('a');
+            if (acceptedName !== ' ' && !(`${person.Name} ${person.Surname}`.toLowerCase().includes(acceptedName.toLowerCase()))) {
+                return;
+            }
+            const card = createElement('a');
             card.className = 'personCard';
-            card.href = `./sub/${person.Name}_${person.Surname}.html`;
+            card.href = `./sub/${person.Name}_${person.Surname}.html`.replaceAll(' ', '_');
 
             card.innerHTML =
                 `
-            <img src="../../images/${person.imageFolder}/medium.jpg" alt="${person.Name} ${person.Surname}">
+            <img src="/src/images/${person.imageFolder}/medium.jpg" alt="${person.Name} ${person.Surname}">
             <div class="personInfo">
                 <h2>${person.Name} ${person.Surname}</h2>
                 <h3>${person.Category}</h3>
@@ -29,3 +32,11 @@ async function read() {
 }
 
 read()
+
+querySelector('#searchInput').addEventListener('keydown', (e) => {
+    if (e.key !== 'Enter')
+        return;
+    const container = querySelector('#cardsContainer');
+    container.innerHTML = '';
+    read(e.target.value.trim());
+});
